d5ae1a4a072df4ebc9c0bc2299c05ece04b62556
OlegFlores <431502+OlegFlores@users.noreply.github.com>
2024-12-26 11:39:36 +0100

    feat(MVP-3): update tg link

--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -5,7 +5,7 @@ export default function ContactsPage() {
         <div className="container w-full flex flex-col lg:flex-row items-center gap-12 mt-14 lg:mt-28">
             <div className="flex flex-col w-full items-center lg:items-start">
                 <p> Hey, my name is Oleg. I developed this thing on Rust and hosted it on my home server. If you have any ideas, suggestions, or just want to chat,
-                    please reach me out via email oyflores&nbsp;(at)&nbsp;gmail.com or in Telegram: <a href="https://t.me/@oyflores">@oyflores</a></p>
+                    please reach me out via email oyflores&nbsp;(at)&nbsp;gmail.com or in Telegram: <a href="https://t.me/oyflores">@oyflores</a></p>
             </div>
         </div>
     );
